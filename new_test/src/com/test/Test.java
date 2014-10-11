3dfb29a0554f54584e3a98edd01f118c69514e23
liufei <liufei19840106@163.com>
2014-10-11 15:47:11 +0800

    init commit

---
+++ new_test/src/com/test/Test.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ */
+package com.test;
+
+/**
+ * @author liufei
+ * TODO
+ * com.test Test.java
+ * 2014年8月21日 下午11:17:54
+ */
+public class Test {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+	}
+	
+}
