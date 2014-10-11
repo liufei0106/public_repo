096a95c8981e2b85156dbf2e3d29dfd437fd1d14
liufei <liufei19840106@163.com>
2014-10-11 15:54:54 +0800

    commit main

---
+++ new_test/src/com/main/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.main;
+
+/**
+ * @author liufei
+ * TODO
+ * com.main Main.java
+ * 2014年10月11日 下午3:53:06
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+	}
+	
+}
