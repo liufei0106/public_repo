c147db0a7a474868b89398cfe34004bb6cac67f4
liufei <liufei19840106@163.com>
2014-10-11 16:32:37 +0800

    dd

--- new_test/src/com/test/Test.java
+++ new_test/src/com/test/Test.java
@@ -15,7 +15,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello,world liufei");
+		System.out.println("hello,world liufei haha");
 	}
 	
 }
